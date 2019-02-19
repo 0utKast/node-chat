b998bf66aedba2962960d3662f6d1cd8f41189c5
0utKast <educaleft@gmail.com>
2019-02-19 09:38:14 +0000

    Chat completo en inglés

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.get("/", function(req, res) {
-  res.send("Hola Mundo");
+app.get('/', function(req, res) {
+    res.render('index.ejs');
+});
+
+io.sockets.on('connection', function(socket) {
+    socket.on('username', function(username) {
+        socket.username = username;
+        io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
+    });
+
+    socket.on('disconnect', function(username) {
+        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+    })
+
+    socket.on('chat_message', function(message) {
+        io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+    });
+
 });
 
 const server = http.listen(8080, function() {
-  console.log("vigilando en *:8080");
+    console.log('listening on *:8080');
 });
